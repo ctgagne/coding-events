ec25e0f7a98703e158d65af32dda6aa50deb5b4d
Chaz Gagne <chazgagne@gmail.com>
2020-05-08 16:15:40 +0200

    create POST through axios for event

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -14,6 +14,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
@@ -22,7 +24,14 @@ export default {
   },
   methods: {
     submit() {
-      console.log(this.event);
+      axios
+        .post("http://localhost:3000/events", this.event)
+        .then(res => {
+          console.log("Created new event", res.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
   }
 };
