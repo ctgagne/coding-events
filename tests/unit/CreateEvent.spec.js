bc6e08105a3f019745dc97f5841fd06ebcd24760
Chaz Gagne <chazgagne@gmail.com>
2020-05-08 10:18:19 +0200

    add unit tests for Create Event component

---
+++ tests/unit/CreateEvent.spec.js
@@ -0,0 +1,13 @@
+import { mount } from "@vue/test-utils";
+import CreateEvent from "@/views/CreateEvent.vue";
+
+describe("CreateEvent", () => {
+  test("it has an <h1> heading", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.contains("h1")).toBe(true);
+  });
+  test("should have a heading saying Create an Event", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.get("h1").text()).toBe("Create an Event");
+  });
+});
