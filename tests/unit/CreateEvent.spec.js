85bd1355c97262d0c1df843529939231ee0bc56b
Chaz Gagne <chazgagne@gmail.com>
2020-05-12 11:03:22 +0200

    all create event tests implemented

--- tests/unit/CreateEvent.spec.js
+++ tests/unit/CreateEvent.spec.js
@@ -1,5 +1,8 @@
 import { mount } from "@vue/test-utils";
 import CreateEvent from "@/views/CreateEvent.vue";
+import { createEvent } from "@/services/event-service.js";
+
+jest.mock("@/services/event-service.js");
 
 describe("CreateEvent", () => {
   test("it has an <h1> heading", () => {
@@ -39,5 +42,33 @@ describe("CreateEvent", () => {
     const titleInput = wrapper.get("input[name='title'");
     titleInput.setValue("JS Pair Programming");
     expect(wrapper.vm.event.title).toBe("JS Pair Programming");
+  });
+
+  test("it should have a submit method", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.vm.submit).toBeDefined();
+    expect(typeof wrapper.vm.submit).toBe("function");
+  });
+
+  test("it should call the submit method on a submit event of the form", () => {
+    const wrapper = mount(CreateEvent);
+    // Add a spy mock function, overiding my method
+    wrapper.vm.submit = jest.fn(); //Creating a mock function
+    wrapper.get("form").trigger("submit");
+    // expect that the spy mock function has been called
+    expect(wrapper.vm.submit).toHaveBeenCalled();
+  });
+
+  test("it should call the event service, after the user has input a title and hit submit", () => {
+    createEvent.mockReset();
+    const wrapper = mount(CreateEvent);
+    // User input a title
+    wrapper.get("input[name='title']").setValue("Go to the zoo");
+    //User hits submit
+    createEvent.mockReturnValue({ data: { title: "", id: 1 } });
+    wrapper.get("form").trigger("submit");
+
+    expect(wrapper.vm.submit).toHaveBeenCalled({ title: "Go to the zoo" });
   });
 });
